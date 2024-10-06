0aa4c19cf4636ec6643f5aeedc5da27260345d66
fawaz <bellofawaz5@gmail.com>
2024-10-06 11:42:05 +0100

    added select and view feature

--- pages/preview.php
+++ pages/preview.php
@@ -2,10 +2,10 @@
 <?php
 include_once("../module/connection.php");
 include_once "../module/handlebarsTemplate.php";
-
+include_once "../module/reactioui.php";
 
 session_start();
-
+$ret;
 if (isset($_GET["id"])) {
   
     $_SESSION["id"] = $_GET["id"];
@@ -19,10 +19,7 @@ if (isset($_GET["id"])) {
 }
 else{
 require dirname(__DIR__) . '\vendor\autoload.php';
-if(!isset($_SESSION["id"])){
-    http_response_code(403);
-    header("location:/anon/403");
-}
+
 $db = new DbConn();
 $db = $db->conn();
 if (!$db) {
@@ -30,7 +27,7 @@ if (!$db) {
     echo "Database connection failed";
     exit;
 }
-
+if(!isset($_GET["selectid"])){
 $id = $_SESSION["id"];
 $userid =     $_SESSION["user"];
 $multi =   $_SESSION["multi"];
@@ -52,16 +49,40 @@ $retexe =$stmt->execute();
      $ret = $stmt->get_result();
 // fetch all rows
 unset($_SESSION["id"],$_SESSION["multi"],$_SESSION["user"]);
-include_once("../module/reactioui.php");
+while ($row = $ret->fetch_array(SQLITE3_ASSOC)) {
+    var_dump($ret->fetch_array(SQLITE3_ASSOC));
+  $reactions = new getReactionUIData($row["USERID"], $row["SENDERID"], $row["MSGID"], ["FUNNY" => $row["FUNNY"], "SAD" => $row["SAD"], "BORING" => $row["BORING"], "CRAZY" => $row["CRAZY"]]);
+  $reactlist = $reactions->getReactionUIData();
+  $data = [...$row, "reactlist" => $reactlist,"preview"=>true];
+  // use a template engine to render the HTML template
+  $handlebars= new HandlebarTemplate(file_get_contents("../public/views/reaction.hbs"));
+  $handlebars->registerHelpers("format");    
+   $template = $handlebars->compile();
+  echo $handlebars->render( $data);
+}
 
+}
+
+else if(isset($_GET)&&isset($_GET["selectid"])){
+    $selectedid = (string)$_GET["selectid"];
+    $query = "SELECT * FROM MESSAGES WHERE MSGID IN (SELECT SELECTED FROM SELECTTABLE WHERE ID =?)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("s",$selectedid);
+    $retexe =$stmt->execute();
+    $ret = $stmt->get_result();
+
+    var_dump($ret->fetch_array(SQLITE3_ASSOC));
     while ($row = $ret->fetch_array(SQLITE3_ASSOC)) {
-      
-    $reactions = new getReactionUIData($row["USERID"], $row["SENDERID"], $row["MSGID"], ["FUNNY" => $row["FUNNY"], "SAD" => $row["SAD"], "BORING" => $row["BORING"], "CRAZY" => $row["CRAZY"]]);
-    $reactlist = $reactions->getReactionUIData();
-    $data = [...$row, "reactlist" => $reactlist,"preview"=>true];
-    // use a template engine to render the HTML template
-    $handlebars= new HandlebarTemplate(file_get_contents("../public/views/reaction.hbs"));
-    $handlebars->registerHelpers("format");    
-     $template = $handlebars->compile();
-    echo $handlebars->render( $data);
-}}
+      $reactions = new getReactionUIData($row["USERID"], $row["SENDERID"], $row["MSGID"], ["FUNNY" => $row["FUNNY"], "SAD" => $row["SAD"], "BORING" => $row["BORING"], "CRAZY" => $row["CRAZY"]]);
+      $reactlist = $reactions->getReactionUIData();
+      $data = [...$row, "reactlist" => $reactlist,"preview"=>true];
+      // use a template engine to render the HTML template
+      $handlebars= new HandlebarTemplate(file_get_contents("../public/views/reaction.hbs"));
+      $handlebars->registerHelpers("format");    
+       $template = $handlebars->compile();
+      echo $handlebars->render( $data);
+  }
+  
+    }}
+
+
