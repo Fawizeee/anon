5ac431264e6d4b20cd06367636da9383bd25fa62
fawaz <bellofawaz5@gmail.com>
2024-10-06 12:56:17 +0100

    ok

--- module/autologin.php
+++ module/autologin.php
@@ -1,14 +1,14 @@
 <?php
 
 class Login {
-    private $db;
-    private $name;
+    private $db; //mysqli
+    private $name; //lobin username
     private $password;
-    private $password_mod;
+    private $password_mod; // module to check password
     private $Coloumn = [
         "username"=>"username",
         "remember"=>"remember"
-    ];
+    ]; //array that holds coloumn name for interchanging
 
     /**
      * Constructor
@@ -16,11 +16,21 @@ class Login {
      * @param string $name
      * @param string $password
      * @param mysqli $db
+     * @param Password_mod $password_mod
+     * @param array $coloumn
      */
     public function __construct( mysqli $db ) {
       
         $this->db = $db;
     }
+
+    /**
+     * Summary of query
+     * @param mysqli $db
+     * @param string $name
+     * @param string $Coloumn
+     * @return bool|mysqli_result
+     */
     public function query(mysqli $db,string $name,string $Coloumn): bool|mysqli_result{
         $query = "
         SELECT
@@ -39,42 +49,55 @@ class Login {
 
         
     }
-
+    /**
+     * Summary of authenticate
+     * @param mysqli_result $result
+     * @param array $credentials
+     * @param Password_mod|null $password_mod
+     * @return array
+     */
     public function authenticate(mysqli_result $result,array $credentials,Password_mod|null $password_mod){
-        
+        // verifies credentials are not empty
         if($password_mod!==null){
         if (empty($credentials["name"]) || empty($credentials["password"])) {
             return ["isUser" => false, "msg" => "Please fill in all fields","row"=>[]];
         }
 }
+        //check if user exists
         try{
         if ($result->num_rows === 0) {
             return ["isUser" => false, "msg" => "User does not exist. Sign up!","row"=>[]];
         }
 
            $row = $result->fetch_assoc();
-
-           var_dump($credentials);
            if($password_mod!=null){
          $verified_password = $password_mod->verifyPassword($row["password"],$credentials["password"]);
-        if ($verified_password) {
+            //check if password is correct
+
+         if ($verified_password) {
+
+            // if it is correct input neccesary sessions and cookies
             $_SESSION["loggedin"] = true;
+            // return user authenticated;
             return ["isUser" => true, "msg" => "", "row" => $row];
         } else {
+            // else wrong paaswoord
             return ["isUser" => false, "msg" => "Wrong password","row"=>[]];
         }
+        //if user data id  matches cookie id authenticate(for autologin)
 }     elseif($row["id"]==$_COOKIE["userid"]){
     return ["isUser" => true, "msg" => "", "row" => $row];
         }
     } catch (mysqli_sql_exception $e) {
+        //display error
         return ["isUser" => false, "msg" => "Database error: " . $e->getMessage(),"row"=>[]];
-    } finally {
-       
     }
+    return [];
     }
     /**
-     * Login
-     *
+     * Summary of login
+     * @param array $credentials
+     * @param Password_mod $password_mod
      * @return array
      */
     public function login(Array $credentials,Password_mod $password_mod): array{
@@ -87,6 +110,11 @@ class Login {
 
          
     }
+    /**
+     * Summary of autologin
+     * @param array $credentials
+     * @return array
+     */
     public function autologin(array $credentials){
        $result = $this->query($this->db,$credentials["remember"],$this->Coloumn["remember"]);
        $authenticated = $this->authenticate($result,$credentials,null);
